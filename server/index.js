483326535a55002d2ce7e127343e5641b968f506
Syed Ramish Hassan <73549298+Ramish93@users.noreply.github.com>
2022-03-17 21:40:40 +0100

    project blog/adventure app completed

---
+++ server/index.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+import postRoutes from './routes/posts.js';
+
+const app = express();
+
+app.use(bodyParser.json({limit: '30mb', extended: true}))
+app.use(bodyParser.urlencoded({limit: '30mb', extended: true}))
+app.use(cors());
+app.use('/posts', postRoutes)
+
+
+//mongo db cloud atlas
+const CONNECTION_URL = 'mongodb+srv://ramish1:ramish123@cluster0.vlrf6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const PORT = process.env.PORT || 5000;
+mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+.then(() => app.listen(PORT, console.log(` Server running on port ${PORT}`)))
+.catch((err) => console.log(err.message));
+
+
+
